37220c1592c16de1a2a28b25fb7459bd6fc3f87c
MuhammadIrsyad95 <irsyadmuhammad339@gmail.com>
2023-01-08 23:04:26 +0700

    add contact

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ function toggleDetail(e) {
   const detail = $(item).children(".about-exp-item-detail");
   $(detail).slideToggle();
 }
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = $("#inp_email");
+  const Subject = $("#inp_Subject");
+  const Message = $("#inp_Message");
+
+  if (!$(email).val()) {
+    alert("Email is required");
+  } else if (!$(subject).val()) {
+    alert("Subject is required");
+  } else if (!$(massage).val()) {
+    alert("Massage is required");
+  } else {
+    $(email).val("");
+    $(subject).val("");
+    $(massage).val("");
+    alert("Form Submitted");
+  }
+}
